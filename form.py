e6699eed641d7dd363cb6ca231e05a73c286a058
ShiyaoGuo2021 <93490345+ShiyaoGuo2021@users.noreply.github.com>
2023-03-30 20:58:20 -0700

    Delete form.py

--- form.py
+++
@@ -1,7 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import IntegerField, SubmitField
-from wtforms.validators import ValidationError, DataRequired
-
-class imgIdForm(FlaskForm):
-	imgid=IntegerField("imageID",validators=[DataRequired()])
-	submit=SubmitField("Search!")
